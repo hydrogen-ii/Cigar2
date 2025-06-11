1e3f02b3d25e1071afcaf7892fe560a9fc1a923f
hydrogen-ii <keplerbinks@gmail.com>
2025-06-11 09:12:10 -0500

    Update webserver.js
    add readability

--- webserver.js
+++ webserver.js
@@ -1,1 +1,8 @@
-const express=require('express'),app=express();app.use(express.static('web'));app.listen(3000);
+const port = 3000;
+
+const express=require('express'),
+  app=express();
+
+app.use(express.static('web'));
+
+app.listen(port, () => console.log(`Cigar2 running at http://localhost:${port}`));
